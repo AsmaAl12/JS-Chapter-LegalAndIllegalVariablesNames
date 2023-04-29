fe981108bca6d6d161d3ff5cc7eb3ef5d03e7ba0
Asma Ali <128274795+AsmaAl12@users.noreply.github.com>
2023-04-29 21:59:30 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,108 @@
+//==================legal variable names=================//
+//A variable name can't contain any spaces.
+//A variable name can contain only letters, numbers, dollar signs, and underscores.
+//====if your variable name consist of more than one words and you want to write legal variable
+// name by placing space between words then you can use camelcase or snakecase(underscorecase) or dollarcase===//
+
+
+
+//==================CamelCase=================//
+//By using camelcase you will be able to write a name consist of more than one words.
+//(1) A camelcase name begins with lowercase.
+//(2) If there's more than one word in the name, each subsequent word gets an initial cap, creating a hump. 
+
+// var myName = "Asma ";
+// var myLastName = "Ali";
+// var myFullName = myName + myLastName;
+// alert ( "My Name is " + myFullName);
+
+// var myDateOfBirth = "6 jan";
+// alert ( "My date of birth " + myDateOfBirth);
+
+// var myHobbies = "writing, reading, learning, playig.";
+// alert ( "My Hobbies are: \n "  + myHobbies);
+
+
+
+//=============snakecase(underscorecase)==================//
+//By using snakecase you will be able to write a name consist of more than one words.
+//(1) In snakecase you will use underscore between words.
+//(2) By using underscore you will be able to understand words more conveniently without giving spaces between them.
+
+// var my_name = "Asma ";
+// var my_last_name = "Ali";
+// var my_full_name = my_name + my_last_name;
+
+// var my_date_of_birth = "6 jan";
+// alert ( "My Date Of Birth is :\n" + my_date_of_birth);
+
+// var my_father_name = "Hassan Ali ";
+// var my_mother_name = "Farukh Naz";
+// var my_parents = my_father_name + " and " + my_mother_name;
+
+// var my_siblings = "Muqadas, Rimsha, Maraj, Sobiya, Hassnain, Saleeqa.";
+// alert ( "My Siblings are : \n" + my_siblings );
+
+
+
+//===============Dollarcase==================//
+//You can also use Dollarcase to write a name consist of more than one words.
+//(1) In snakecase you will use $ instead of using underscore between words.
+//(2) By using $ you will be able to understand words more convenently without giving spaces between them.
+
+// var my$name = "Asma ";
+// var my$last$name = "Ali";
+// var my$full$name = my$name + my$last$name;
+
+// var my$date$of$birth = "6 jan";
+// alert ( "My Date Of Birth is :\n" + my$date$of$birth);
+
+// var my$father$name = "Hassan Ali ";
+// var my$mother$name = "Farukh Naz";
+// var my$parents = my$father$name + " and " + my$mother$name;
+
+// var my$siblings = "Muqadas, Rimsha, Maraj, Sobiya, Hassnain, Saleeqa.";
+// alert ( "My Siblings are : \n" + my$siblings );
+
+
+
+//========================Variables Names With Numbers======================//
+//You can also includes numbers between words but you can't begin a variable name 
+//with a number because if you do this then the variable name will be illegal.
+
+// var my1stProject = "Science ";
+// var my2ndProject = "Mathematics";
+
+
+
+
+
+
+
+
+
+
+//============================Illegal Variables Names========================/
+//Illegal variables names are created when we use special characters like (!, @, #, %, ^, &, *) or when we put space between words.
+//Here are the few examples of illegal variables names:
+
+// var my name = "ASMA";
+// alert ( my name);
+
+// var my!name = "Asma";
+// my!name = "Achnu";
+// alert ( my!name);
+
+// var my@name = "Asma";
+// my@name = "Achnu";
+// alert ( my@name);
+
+// var my#name = "Asma";
+// my#name = "Achnu";
+// alert ( my#name);
+
+// var my%name = "Asma";
+// my%name = "Achnu";
+// alert ( my%name);
+
+//=============================================================================//
